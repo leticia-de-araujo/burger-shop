aebfbcf8a7319711f6355b48a72ac2fb71e902c1
Letícia De Araújo Nunes <leticiadearaujonunes@gmail.com>
2022-06-09 15:30:38 -0300

    entrega pronta

---
+++ hamburgueria-kenzie/src/components/Header/Logo/index.jsx
@@ -0,0 +1,12 @@
+import logo from "./../../../img-icons/logo.svg";
+import "./style.css";
+
+const Logo = () => {
+  return (
+    <div className="Logo-div">
+      <img src={logo} alt="Logo Burguer Kenzie" />
+    </div>
+  );
+};
+
+export default Logo;
